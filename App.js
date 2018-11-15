7198c143f0f34dd444511ed6e05d48fe577b634b
OctavioB <octaviob@nimble.la>
2018-11-15 02:23:06 -0300

    ADD: login header

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
-import { StyleSheet, SafeAreaView } from 'react-native';
 import FlatsList from './src/components/FlatsList';
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <FlatsList />
-      </SafeAreaView>
+      <FlatsList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
--- src/components/FlatsList.js
+++ src/components/FlatsList.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 
 export default class FlatsLists extends Component {
   render() {
     return (
-      <View>
+      <SafeAreaView style={styles.container}>
         <View style={styles.titleContainerStyle}>
-          <Text>Hire Flats!</Text>
+          <Text style={styles.titleTextStyle}>Hire Flats!</Text>
         </View>
         <View style={styles.flatsListContainerStyle} />
-      </View>
+      </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
   titleContainerStyle: {
-    flex: 3
+    alignItems: 'center',
+    backgroundColor: '#8DDD00',
+    flex: 1,
+    justifyContent: 'center'
+  },
+  titleTextStyle: {
+    fontWeight: '900'
   },
   flatsListContainerStyle: {
-    flex: 8
+    flex: 10
   }
 });
